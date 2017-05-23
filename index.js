588d579ea5f0858c23fcacfa4354729f965ec3c1
David Shustin <david.shustin@gmail.com>
2017-05-23 11:53:33 -0400

    Restructure script

--- index.js
+++ index.js
@@ -10,26 +10,13 @@ const scopes = 'playlist-read-private';
 var app = express();
 //This application uses Spotify's implicit code flow authorization.
 var tokenReceived = false;
-function run(token) {
-  const userInfoOptions = {
-    hostname: 'api.spotify.com',
-    path: '/v1/me',
-    method: 'GET',
-    headers: {
-      'Authorization': 'Bearer ' + token
-    }
-  };
-  // console.log(userInfoOptions.headers['Authorization']);
-  const userInfoReq = https.request(userInfoOptions, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    })
-  })
-
-  userInfoReq.on('error', (err) => console.error(error('There was an error with the user info request: %s'), e.message));
-  userInfoReq.end();
+try {
+  const clientData = JSON.parse(fs.readFileSync('./config.json').toString());
+} catch (e) {
+  console.error(error('Error opening %s'), e.path); return;
 }
+const state = Math.floor(Math.random() * Math.pow(2, 32));
+const query = '?client_id=' + clientData.client_id + '&response_type=token&redirect_uri=http://localhost:' + responsePort + '/redir&state=' + state + '&scope=' + encodeURI(scopes) + '';
 app.get('/redir', (req, res) => {
   res.sendFile(__dirname + '/public/redir.html');
 });
@@ -51,13 +38,25 @@ app.get('/auth', (req, res) => {
   run(req.query.access_token);
   // app.close();
 });
-try {
-  const clientData = JSON.parse(fs.readFileSync('./config.json').toString());
-} catch (e) {
-  console.error(error('Error opening %s'), e.path); return;
-}
-const state = Math.floor(Math.random() * Math.pow(2, 32));
-const query = '?client_id=' + clientData.client_id + '&response_type=token&redirect_uri=http://localhost:' + responsePort + '/redir&state=' + state + '&scope=' + encodeURI(scopes) + '';
 app.listen(responsePort, () => console.log(chalk.green.dim('Listening for OAuth token authorizaton on port', responsePort)));
 console.log('Opening web browser to authorize...');
 opn('https://accounts.spotify.com/authorize' + query);
+function getID(token) {
+  const userInfoOptions = {
+    hostname: 'api.spotify.com',
+    path: '/v1/me',
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  };
+  // console.log(userInfoOptions.headers['Authorization']);
+  const userInfoReq = https.request(userInfoOptions, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    })
+  })
+  userInfoReq.on('error', (err) => console.error(error('There was an error with the user info request: %s'), e.message));
+  userInfoReq.end();
+}
